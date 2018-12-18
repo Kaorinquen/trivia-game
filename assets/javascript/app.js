9e9a091a0f701a2f2d380230fc5d6ce181337c8f
JovanJSYK <40006077+Kaorinquen@users.noreply.github.com>
2018-12-18 16:59:31 -0500

    for loop and html added

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+document.ready() {
+
+
+var sfAnswers = ["Chun Li", "Blanka", "Kevin Nash", "E. Honda"];
+var rapAnswers = ["Kendrick Lamar", "J. Cole", "KRS-ONE", "21 Savage"];
+
+var questionForm = '<div class="form-check">' +
+'<input class="form-check-input" type="radio"  value="option[i]">' +
+'<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
+'</div>';
+
+var gameGuts = function (game){
+for (i=0; i < 4; i++) {
+    $(questionForm).(indexOf[i]);
+
+
+        }
+
+    }
+
+}
