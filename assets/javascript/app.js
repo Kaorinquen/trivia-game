b07e6b3f4b81baae571151eecdce1d6231e86944
Kaorinquen <jovan@myradar.com>
2018-12-19 22:03:46 -0500

    expanded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,30 @@
-document.ready() {
+document.ready(function() {
+
+    var count = 0;
+    var timer = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
 
+
+var questions = ["Which is these are NOT a Street Fighter","This rapper was recently awared a Pulitzer Prize",""]
+var correctAnswers = ["Kevin Nash","Kendrick Lamar"]
 var sfAnswers = ["Chun Li", "Blanka", "Kevin Nash", "E. Honda"];
 var rapAnswers = ["Kendrick Lamar", "J. Cole", "KRS-ONE", "21 Savage"];
+var firstChoice = ["Chun Li","Kendrick Lamar"];
+var secondChoice = ["Blanka","J. Cole"];
+var thirdChoice = ["Kevin Nash","KRS-ONE"];
+var fourthChoice = ["E. Honda", "21 Savage"];
 
-var questionForm = '<div class="form-check">' +
-'<input class="form-check-input" type="radio"  value="option[i]">' +
-'<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
-'</div>';
+// var questionForm = '<div class="form-check">' +
+// '<input class="form-check-input" type="radio"  value="option[i]">' +
+// '<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
+// '</div>';
 
 var gameGuts = function (game){
 for (i=0; i < 4; i++) {
-    $(questionForm).(indexOf[i]);
+    $(questionForm).html(indexOf[i]);
 
 
         }
@@ -19,3 +32,4 @@ for (i=0; i < 4; i++) {
     }
 
 }
+
