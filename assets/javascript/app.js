d23865ed7e57af8a5ff626c450c9d669e63cda5a
Kaorinquen <jovan@myradar.com>
2018-12-19 23:31:06 -0500

    answer slots added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-document.ready(function() {
+$(document).ready(function() {
 
     var count = 0;
     var timer = 0;
@@ -8,8 +8,8 @@ document.ready(function() {
 
 
 
-var questions = ["Which is these are NOT a Street Fighter","This rapper was recently awared a Pulitzer Prize",""]
-var correctAnswers = ["Kevin Nash","Kendrick Lamar"]
+var questions = ["Which is these are NOT a Street Fighter","This rapper was recently awared a Pulitzer Prize",""];
+var correctAnswers = ["Kevin Nash","Kendrick Lamar"];
 var sfAnswers = ["Chun Li", "Blanka", "Kevin Nash", "E. Honda"];
 var rapAnswers = ["Kendrick Lamar", "J. Cole", "KRS-ONE", "21 Savage"];
 var firstChoice = ["Chun Li","Kendrick Lamar"];
@@ -22,14 +22,31 @@ var fourthChoice = ["E. Honda", "21 Savage"];
 // '<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
 // '</div>';
 
-var gameGuts = function (game){
-for (i=0; i < 4; i++) {
-    $(questionForm).html(indexOf[i]);
 
 
-        }
-
-    }
-
+function (showQA) {
+    $("#questionHere").show();
+    $("AnswerSlot1").show();
+    $("AnswerSlot2").show();
+    $("AnswerSlot3").show();
+    $("AnswerSlot4").show();
 }
 
+function (hideQA){
+    $("#questionHere").hide();
+    $("AnswerSlot1").hide();
+    $("AnswerSlot2").hide();
+    $("AnswerSlot3").hide();
+    $("AnswerSlot4").hide();
+}
+
+function (questionSlide){
+    $("#questionHere").html(questions[count]);
+    $("AnswerSlot1").html(firstChoice[count]);
+    $("AnswerSlot2").html(secondChoice[count]);
+    $("AnswerSlot3").html(thirdChoice[count]);
+    $("AnswerSlot4").html(fourthChoice[count]);
+}
+
+
+}
