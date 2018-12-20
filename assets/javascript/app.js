8b07cc14247cec3e044c7b42ee7fb13c13232f32
JovanJSYK <40006077+Kaorinquen@users.noreply.github.com>
2018-12-20 16:58:06 -0500

    expanded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(); {
 
     var count = 0;
-    var timer = 0;
+    var timer = 30;
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
 
 
-
+var startButton = $("#sutaato")
 var questions = ["Which is these are NOT a Street Fighter","This rapper was recently awared a Pulitzer Prize",""];
 var correctAnswers = ["Kevin Nash","Kendrick Lamar"];
 var sfAnswers = ["Chun Li", "Blanka", "Kevin Nash", "E. Honda"];
@@ -16,37 +16,49 @@ var firstChoice = ["Chun Li","Kendrick Lamar"];
 var secondChoice = ["Blanka","J. Cole"];
 var thirdChoice = ["Kevin Nash","KRS-ONE"];
 var fourthChoice = ["E. Honda", "21 Savage"];
+var userAnswer= [""]
 
 // var questionForm = '<div class="form-check">' +
 // '<input class="form-check-input" type="radio"  value="option[i]">' +
 // '<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
 // '</div>';
 
+// var x = setTimeout(thirtySeconds, 1000 * 30)
 
-
-function (showQA) {
+//show all q&a's on page
+function showQA() {
+    // $(timer).html();
     $("#questionHere").show();
     $("AnswerSlot1").show();
     $("AnswerSlot2").show();
     $("AnswerSlot3").show();
     $("AnswerSlot4").show();
 }
-
-function (hideQA){
+//hide q&a's (for game start and after answring question)
+function hideQA() {
     $("#questionHere").hide();
     $("AnswerSlot1").hide();
     $("AnswerSlot2").hide();
     $("AnswerSlot3").hide();
     $("AnswerSlot4").hide();
 }
-
-function (questionSlide){
-    $("#questionHere").html(questions[count]);
-    $("AnswerSlot1").html(firstChoice[count]);
-    $("AnswerSlot2").html(secondChoice[count]);
-    $("AnswerSlot3").html(thirdChoice[count]);
-    $("AnswerSlot4").html(fourthChoice[count]);
+//creates a new "slide"; empties page and shows new info
+function questionSlide() {
+    // $("#timeHere").text(timer);
+    startButton.hide;
+    $("#questionHere").text(questions[count]);
+    $("#AnswerSlot1").text(firstChoice[count]);
+    $("#AnswerSlot2").text(secondChoice[count]);
+    $("#AnswerSlot3").text(thirdChoice[count]);
+    $("#AnswerSlot4").text(fourthChoice[count]);
+    console.log(questions[count]);
 }
 
+function gameStart() {
+    showQA();
+    questionSlide();
+    console.log(questionSlide);
+}
+$("#sutaato").on("click", gameStart())
 
 }
