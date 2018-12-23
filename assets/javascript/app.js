7c1bd2d9336141ae9eb8f6632abf0c3f88c7aa09
Kaorinquen <jovan@myradar.com>
2018-12-23 16:08:38 -0500

    function expanded, questions built

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,19 @@
-// on each new "slide", the code needs to:
-
-// show the timer and start a countdown
-// make the start button disappear
-// show the question
-// make each button an answer (which will have some sort of hover state) and show then in order
-
-// Selecting an answer will:
-// Clear the page
-// Restart the time
-// Show the next question
-// Display the next set of answers
-
-// At game's end:
-// Show number of correct answers
-// show number of wrong answers
-// play again button
-// go back to start screen
 
 
 
-$(document).ready(); {
 
     var count = 0;
-    var timer = 30;
+    var timer = 31;
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
 
+// show the timer and start a countdown from 30 done
+// make the start button disappear 
+// show the question
+// make each button an answer (which will have some sort of hover state) and show then in order
 
-var startButton = $("#sutaato")
+var startButton = $("#sutaato");
 var questions = ["Which is these are NOT a Street Fighter","This rapper was recently awared a Pulitzer Prize",""];
 var correctAnswers = ["Kevin Nash","Kendrick Lamar"];
 var sfAnswers = ["Chun Li", "Blanka", "Kevin Nash", "E. Honda"];
@@ -39,6 +24,16 @@ var thirdChoice = ["Kevin Nash","KRS-ONE"];
 var fourthChoice = ["E. Honda", "21 Savage"];
 var userAnswer= [""]
 
+$(document).ready(); {
+
+var countdownTimer = setInterval(function(){
+    timer--;
+    $("#timeHere").text("Time Left: " + timer);
+    if(timer <= 0)
+        clearInterval(countdownTimer);
+    },1000);
+
+
 // var questionForm = '<div class="form-check">' +
 // '<input class="form-check-input" type="radio"  value="option[i]">' +
 // '<label class="form-check-label" for="exampleRadios1" id="sfAnswers"></label>' +
@@ -47,8 +42,20 @@ var userAnswer= [""]
 // var x = setTimeout(thirtySeconds, 1000 * 30)
 
 //show all q&a's on page
+
+function questionSlide() {
+    // $("#timeHere").text(timer);
+    startButton.hide();
+    $("#questionHere").text(questions[count]).addId();
+    $("#AnswerSlot1").text(firstChoice[count]);
+    $("#AnswerSlot2").text(secondChoice[count]);
+    $("#AnswerSlot3").text(thirdChoice[count]);
+    $("#AnswerSlot4").text(fourthChoice[count]);
+    console.log(questions[count]);
+}
+
 function showQA() {
-    // $(timer).html();
+    $(countdownTimer).html();
     $("#questionHere").show();
     $("AnswerSlot1").show();
     $("AnswerSlot2").show();
@@ -63,28 +70,31 @@ function hideQA() {
     $("AnswerSlot3").hide();
     $("AnswerSlot4").hide();
 }
-//creates a new "slide"; empties page and shows new info
-function questionSlide() {
-    // $("#timeHere").text(timer);
-    $("#sutaato").hide;
-    $("#questionHere").text(questions[count]);
-    $("#AnswerSlot1").text(firstChoice[count]);
-    $("#AnswerSlot2").text(secondChoice[count]);
-    $("#AnswerSlot3").text(thirdChoice[count]);
-    $("#AnswerSlot4").text(fourthChoice[count]);
-    console.log(questions[count]);
-}
 
+
+//function for priming click listening
 function gameStart() {
-    showQA();
     questionSlide();
+    showQA();
     console.log(questionSlide);
 }
-if ($("#sutaato").on("click")) {
- 
-gameStart();
- questionSlide();
+// Selecting an answer will:
+// Clear the page
+// Restart the time
+// Show the next question
+// Display the next set of answers
+
+if (startButton.on("click")) {
+    this.hide();
+    gameStart();
+    questionSlide();
  console.log(question[count]);
 }
 
-}
+
+
+// At game's end:
+// Show number of correct answers
+// show number of wrong answers
+// play again button
+// go back to start screen
